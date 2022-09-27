baaa53f51d655ee194ce5efcce2bda5fab2c2c6e
Pawan Gupta <pawan-crestron>
2022-09-27 23:23:12 +0530

    First commit.

---
+++ src/main/java/com/kyn/gateway/KynGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.kyn.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KynGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KynGatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kyn/gateway/KynGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kyn.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KynGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
