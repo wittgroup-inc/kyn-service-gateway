eb5af350d0ff24ccd0d749e80d0221ceb4a433cb
Pawan Gupta <pawan-crestron>
2022-10-11 00:36:16 +0530

    Security configuration using keycloak.

---
+++ src/main/java/com/wittgroup/kyn/gateway/SecurityConfig.java
@@ -0,0 +1,20 @@
+package com.wittgroup.kyn.gateway;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+@EnableWebFluxSecurity
+public class SecurityConfig {
+    @Bean
+    public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity serverHttpSecurity) {
+        serverHttpSecurity.authorizeExchange(exchanges -> exchanges.anyExchange().authenticated()).oauth2Login(Customizer.withDefaults());
+        serverHttpSecurity.csrf().disable();
+        return serverHttpSecurity.build();
+
+    }
+}
